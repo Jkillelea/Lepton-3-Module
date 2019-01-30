7f378c57e183f7c9117f0741f388f5bc8a95866e
Jacob Killelea <Jkillelea@protonmail.ch>
2019-01-29 20:01:42 -0700

    misc

--- scripts/parse_txt_image.py
+++ scripts/parse_txt_image.py
@@ -12,15 +12,15 @@ filename = sys.argv[1]
 
 with open(filename, "r") as f:
     row = 0
-    for line in f.read().split("\n"):
+    for line in f.read().strip().split("\n"):
         try:
             numbers = [float(i) for i in line.strip().split(" ")]
             data[row, :] = numbers
         except Exception as e:
-            print(e)
+            print("Excption: {}".format(e))
         row += 1
 
-print(data)
+# print(data)
 
 fig = plt.figure()
 

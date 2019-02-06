409e031bb9cda6fbd367f7b60be99116d46a6f4d
Jacob Killelea <Jkillelea@protonmail.ch>
2019-02-06 12:06:05 -0700

    misc

--- software/raw_capture/raw_capture.c
+++ software/raw_capture/raw_capture.c
@@ -89,6 +89,9 @@ void read_image(uint16_t *data_ptr) {
             packet_number  = ((packet[0] & 0x0f) << 4) 
                              | packet[1];
 
+            if (packet_number > 60)
+                continue;
+
             if (packet_number != pak)
                 pak = packet_number;
 

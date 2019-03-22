53fce0f353e5fcb5593f49034834f50cc1a6bf5c
Jacob Killelea <Jkillelea@protonmail.ch>
2019-03-22 13:51:39 -0600

    use error decoding function from SDK

--- software/double_video/Lepton_I2C.cpp
+++ software/double_video/Lepton_I2C.cpp
@@ -19,10 +19,11 @@ LEP_RESULT result;
 int default_i2c_num = 1;
 
 int lepton_connect(int i2c_num) {
-	int res = (int) LEP_OpenPort(i2c_num, LEP_CCI_TWI, 400, &_port);
+	LEP_RESULT res = LEP_OpenPort(i2c_num, LEP_CCI_TWI, 400, &_port);
     if (res != LEP_OK) {
         qDebug() << "Failed to connect!";
-        qDebug() << "error code: " << res << "";
+        // qDebug() << "error code: " << res << "";
+        qDebug() << "error code: " << error_as_string(res);
     } else {
         _connected = true;
     }
@@ -33,10 +34,10 @@ int lepton_enable_radiometry() {
     if (!_connected)
         lepton_connect(default_i2c_num);
 
-    int res = LEP_SetRadEnableState(&_port, LEP_RAD_ENABLE);
+    LEP_RESULT res = LEP_SetRadEnableState(&_port, LEP_RAD_ENABLE);
     if (res != LEP_OK) {
         qDebug() << "Failed to enable radiometry";
-        qDebug() << "error code: " << res << "";
+        qDebug() << "error code: " << error_as_string(res) << "";
     }
     return res;
 }
@@ -59,18 +60,16 @@ float raw2Celsius(float raw){
 void lepton_perform_ffc() {
 	qDebug() << "performing FFC...";
 	if(!_connected) {
-		int res = lepton_connect(default_i2c_num);
-		if (res != 0) {
-			//check SDA and SCL lines if you get this error
+        //check SDA and SCL lines if you get this error
+		if (lepton_connect(default_i2c_num) != LEP_OK) {
 			qDebug() << "I2C could not connect";
-			qDebug() << "error code: " << res;
 		}
 	}
 
-    int res = (int)LEP_RunSysFFCNormalization(&_port);
+    LEP_RESULT res = LEP_RunSysFFCNormalization(&_port);
     if (res != 0) {
         qDebug() << "FFC not successful";
-        qDebug() << "error code: " << res;
+        qDebug() << "error code: " << error_as_string(res);
     } else {
         qDebug() << "FFC successful";
     }
@@ -78,19 +77,17 @@ void lepton_perform_ffc() {
 
 void lepton_restart() {
 	if(!_connected) {
-		int res = lepton_connect(default_i2c_num);
-		if (res != 0) {
+		if (lepton_connect(default_i2c_num) != LEP_OK) {
 			//check SDA and SCL lines if you get this error
 			qDebug() << "I2C could not connect";
-			qDebug() << "error code: " << res;
 		}
 	}
 
 	qDebug() << "restarting...";
-	Result res = LEP_RunOemReboot(&_port);
+	LEP_RESULT res = LEP_RunOemReboot(&_port);
 
 	if(res != LEP_OK)
-		qDebug() << "restart unsuccessful with error: " << res;
+		qDebug() << "restart unsuccessful with error: " << error_as_string(res);
 	else
 		qDebug() << "restart successful!";
 }

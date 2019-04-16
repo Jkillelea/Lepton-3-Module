34c68692b063cd80788bb2ac43698f3805a73fcf
Jacob Killelea <Jkillelea@protonmail.ch>
2019-04-16 13:20:10 -0600

    up to date w/ rpi

--- software/raw_capture/raw_capture.c
+++ software/raw_capture/raw_capture.c
@@ -28,8 +28,8 @@
 #include "packet.h"
 #include "segment.h"
 
-// #define LOG(...)
-#define LOG(...) fprintf(stderr, __VA_ARGS__)
+#define LOG(...)
+// #define LOG(...) fprintf(stderr, __VA_ARGS__)
 
 // I2C vars
 uint16_t i2c_number = -1;
@@ -37,7 +37,7 @@ LEP_CAMERA_PORT_DESC_T i2c_port;
 
 // SPI protocol vars
 int     spi_fd    = -1;
-int     spi_speed = 20000000;
+int     spi_speed = 15000000;
 uint8_t spi_mode  = SPI_MODE_3;
 char    spi_path[255];
 uint8_t spi_bits_per_word = 8;
@@ -150,7 +150,7 @@ int main(int argc, char *argv[]) {
     spi_fd = open_spi_port(spi_path);
 
     // Read the image
-    for (uint32_t i = 0; i < 1; i++) {
+    for (uint32_t i = 0; i < 5; i++) {
         read_image();
     }
 
@@ -164,26 +164,10 @@ int main(int argc, char *argv[]) {
     for (uint32_t seg = 0; seg < 4; seg++)
         transmission_ok = transmission_ok && (segments[seg].segment_no == seg+1);
 
-    bool no_zeros = true;
-    for (uint32_t seg = 0; seg < 4; seg++) {
-        segment_t *segment = &segments[seg];
-        for (uint32_t pak = 0; pak < 60; pak++) {
-            packet_t *packet = &segment->packets[pak];
-            for (uint32_t i = 0; i < 80; i++) {
-                no_zeros = no_zeros && !(packet->data[i] == 0);
-            }
-        }
-    }
-
-    if (no_zeros)
-        LOG("Some pixels were zero\n");
-    else
-        LOG("Picture looks good...\n");
-
     if (transmission_ok)
-        LOG("OK\n");
+        fprintf(stderr, "OK\n");
     else
-        LOG("ERROR\n");
+        fprintf(stderr, "ERROR\n");
 
     for (uint32_t seg = 0; seg < 4; seg++) {
         segment_t *segment = &segments[seg];
